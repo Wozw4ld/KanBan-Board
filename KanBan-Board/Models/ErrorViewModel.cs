3955ab315e08dadec779432d33dc9e6c63e2094f
Almaz Saparbaev <almaz.0.saparbaev@gmail.com>
2025-01-27 00:04:17 +0600

    Add project files.

---
+++ KanBan-Board/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KanBan_Board.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
